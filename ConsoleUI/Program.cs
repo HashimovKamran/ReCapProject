9f2c8ba65bc93f59c29468d6a609d8658851691d
Hp <kamranhesimov9@gmail.com>
2021-02-07 23:42:18 +0400

    Добавьте файлы проекта.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Concrete.InMemory;
+using Business.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Id: " + car.Id + " BrandId: " + car.BrandId + " ColorId: " + car.ColorId + " Model Year: " + car.ModelYear + " Daily Price: " + car.DailyPrice + " Description: " + car.Description);
+            }
+        }
+    }
+}
